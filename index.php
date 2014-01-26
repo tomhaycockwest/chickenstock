1784603509fd045da3b65f574650839523a4b177
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-01-26 13:59:01 +0000

    added php includes to site

---
+++ index.php
@@ -0,0 +1,123 @@
+<?php include 'header.php'; ?>
+
+	<body>
+		<div id="wrapper">
+			<?php include 'nav.php' ?>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 column">
+						<div class="title">Chicken</div>
+						<ul class="item-list">
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,150g</div>
+									<div class="price decrease">&pound;6.59</div>
+								</div>
+							</li>
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,150g &lt; 1,350g</div>
+									<div class="price decrease">&pound;2.30</div>
+								</div>
+							</li>
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,350g &lt; 1,550g</div>
+									<div class="price increase">&pound;7.11</div>
+								</div>
+							</li>
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,150g &lt; 2,050g</div>
+									<div class="price increase">&pound;3.89</div>
+								</div>
+							</li>
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Roasters</div>
+									<div class="weight">2,050g &lt; 2,450g</div>
+									<div class="price decrease">&pound;4.61</div>
+								</div>
+							</li>
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Roasters</div>
+									<div class="weight">2,450g &gt;</div>
+									<div class="price decrease">&pound;7.11</div>
+								</div>
+							</li>
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Skinless Breast Fillets</div>
+									<div class="price increase">&pound;2.12</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-3 column">
+						<div class="title">Turkey</div>
+						<ul class="item-list">
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,150g</div>
+									<div class="price decrease">&pound;6.59</div>
+								</div>
+							</li>
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,150g &lt; 1,350g</div>
+									<div class="price decrease">&pound;2.30</div>
+								</div>
+							</li>
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,350g &lt; 1,550g</div>
+									<div class="price increase">&pound;7.11</div>
+								</div>
+							</li>
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,150g &lt; 2,050g</div>
+									<div class="price increase">&pound;3.89</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-3 column">
+						<div class="title">Duckling</div>
+						<ul class="item-list">
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,150g</div>
+									<div class="price decrease">&pound;6.59</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-3 column">
+						<div class="title">Goose</div>
+						<ul class="item-list">
+							<li class="item">
+								<div class="item-container">	
+									<div class="name">Broilers</div>
+									<div class="weight">1,150g</div>
+									<div class="price decrease">&pound;6.59</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include 'footer.php'; ?>
+	</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,14 @@
+<div class="nav">
+	<img id="logo" src="img/logo.png">
+	<ul id="nav-items">
+		<li>
+			<img src="img/Overview-selected.png">
+		</li>
+		<li>
+			<img src="img/price-chart.png">
+		</li>
+		<li class="about-link">
+			<img src="img/About.png">
+		</li>
+	</ul>
+</div>
