f85405797cfcc43ca2e9d63ce9c97980f4d89e35
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-04-28 16:23:12 +0100

    move scripts

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    
 
 
    <script type="text/javascript">
