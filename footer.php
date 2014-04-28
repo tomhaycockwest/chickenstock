78428adbda6d8111dcb921def1898428797765d5
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-04-28 16:12:20 +0100

    fix overview layout issues in firefox

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
 <script type="text/javascript" src="js/jqplot.dateAxisRenderer.min.js"></script>
