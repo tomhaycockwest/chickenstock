35d8e33860283d6d4eee1aa4b8161c0b88fdd3a8
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-05-10 15:52:12 +0100

    detect for windows phone and android tablet

--- browser.php
+++ browser.php
@@ -1,12 +1,15 @@
 <?php
 $iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
-$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android" && "Mobile");
 $palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
 $berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
 $ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$windowsPhone = strpos($_SERVER['HTTP_USER_AGENT'],"phone");
 $ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
+$androidTablet = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
 
-if ($iphone || $android || $palmpre || $ipod || $berry == true) 
+
+if ($iphone || $android || $palmpre || $ipod || $windowsPhone || $berry == true) 
 { 
 header('http://chickenstockapp.com/index-mobile.php');
 //OR
@@ -14,7 +17,7 @@ echo "<script>window.location='http://chickenstockapp.com/index-mobile.php'</scr
 }
 
 
-if ($ipad == true) 
+if ($ipad || $androidTablet == true) 
 { 
 header('http://chickenstockapp.com/index-tablet.php');
 //OR
