56fa3a94cedd35933c233bbbf7f5372b49358ae7
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-05-14 20:41:14 +0100

    add bu logo

--- about.php
+++ about.php
@@ -3,7 +3,9 @@
 		<button type="button" class="close" aria-hidden="true">&times;</button>
 		<h3>Chicken Stock</h3>
 		<p>Chicken Stock is an online tool that allows catering butchers to check the latest wholesale poultry prices at Smithfield Market. This web app allows users to access live prices from a desktop, tablet or mobile device, giving users the opportunity to buy the best quality poultry at competitive prices. Use the overview page to view the latest prices in the market. Select the price chart page to check the current prices against previous years.</p>
-		
+		<div id="bu_logo">
+			<img src="img/bu-logo.png">
+		</div>
 		<p id="source">Data Source: <a href="https://www.gov.uk/government/statistical-data-sets/commodity-prices" target="_BLANK">DEFRA</a></p>
 	</div>
 </div>
